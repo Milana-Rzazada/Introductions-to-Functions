cfea75da6700582bfb312b38de893f6b56715a4d
Milana-Rzazade <71l1c0h@code.edu.az>
2025-06-16 12:58:25 -0400

    Introduction to Functions

---
+++ script.js
@@ -0,0 +1,71 @@
+console.log("NAME FUNCTION STARTS: ");
+function greet(name){
+    return "Hello, " + name + "!";
+}
+
+console.log(greet("Milana"));
+console.log("NAME FUNCTION ENDS: ");
+
+
+
+console.log("addNumbers function starts: ");
+function addNumbers(num1, num2){
+    return num1 + num2;
+
+}
+
+console.log("SUM =", addNumbers(5,9));
+console.log("SUM =", addNumbers(45,12));
+console.log("SUM =", addNumbers(14,36));
+console.log("SUM =", addNumbers(78,69));
+console.log("Multiplication = ", addNumbers(6,9));
+console.log("Division = ", addNumbers(12,6));
+console.log("Subtraction = ", addNumbers(25,12));
+
+
+console.log("addNumbers function ends: ");
+
+console.log("Global and Local variable starts:");
+
+let a = 25;
+console.log("Global: ", a);
+
+
+function changeValue(){
+    let b = 50;
+    let c = 13;
+    console.log("Local: ", b);
+    console.log("Local: ", c);
+
+}
+
+console.log("Global and Local variables ends:");
+
+
+console.log("Cosure funsction: ");
+function outerFunction(){
+    let count=0;
+    return function(){
+        count++;
+        console.log("Count: ", count);
+    }
+}
+
+const counter = outerFunction();
+counter();
+counter();
+counter();
+counter();
+counter();
+counter();
+counter();
+counter();
+counter();
+counter();
+counter();
+counter();
+
+console.log("Global and Local ends:");
+
+
+
